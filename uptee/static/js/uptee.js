bb05a1839d259b967ccc75604a8db51b2f252bb5
SushiTee <weichel.sascha@xxx.xx>
2012-10-19 20:37:45 +0200

    close buttons

--- uptee/static/js/uptee.js
+++ uptee/static/js/uptee.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
     $('.cycle').cycle({fx:'scrollDown',easing:'easeOutBounce',pause:1});
+
+    $('.notification_close').click(function() {
+        $(this).parent().animate({opacity: 0, height: 0, padding: 0}, "fast", function() {
+            $(this).remove();
+        });
+    });
 });
