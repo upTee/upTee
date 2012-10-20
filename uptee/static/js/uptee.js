06b178f3d1e2745c11ebd6dd98d99b916e26005c
SushiTee <weichel.sascha@xxx.xx>
2012-10-20 14:00:20 +0200

    made animation look like i planned it to look like

--- uptee/static/js/uptee.js
+++ uptee/static/js/uptee.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('.cycle').cycle({fx:'scrollDown',easing:'easeOutBounce',pause:1});
 
     $('.notification_close').click(function() {
-        $(this).parent().animate({opacity: 0, height: 0, padding: 0}, "fast", function() {
+        $(this).parent().animate({opacity: 0, height: 0, paddingTop: 0, paddingBottom: 0, marginTop: 0}, 300, function() {
             $(this).remove();
         });
     });
