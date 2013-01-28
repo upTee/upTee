d8a1082396af40bbc533cb1c6688edfc90456b64
SushiTee <weichel.sascha@xxx.xx>
2013-01-28 16:58:22 +0100

    reintegrated generic views

--- uptee/urls.py
+++ uptee/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.defaults import page_not_found, permission_denied, server_error
+from django.views.generic.simple import direct_to_template
 import settings
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'views.home', name='home'),
     url(r'^', include('mod.urls')),
     url(r'^', include('accounts.urls')),
     url(r'^', include('messaging.urls')),
-    url(r'^about/$', 'views.about', name='about'),
+    url(r'^about/$', direct_to_template, {'template': 'about.html'}, name='about'),
     url(r'^captcha/', include('captcha.urls')),
     (r'^admin/', include(admin.site.urls)),
     (r'^admin_tools/', include('admin_tools.urls')),
@@ -19,13 +20,8 @@ urlpatterns = patterns('',
 urlpatterns += staticfiles_urlpatterns()
 
 if settings.DEBUG:
-    urlpatterns += patterns('views',
-        (r'^403/$', 'custom_permission_denied'),
-        (r'^404/$', 'custom_page_not_found'),
-        (r'^500/$', 'custom_server_error'),
+    urlpatterns += patterns('django.views.defaults',
+        (r'^403/$', page_not_found),
+        (r'^404/$', permission_denied),
+        (r'^500/$', server_error),
     )
-
-# custom views for error pages
-handler403 = 'views.custom_permission_denied'
-handler404 = 'views.custom_page_not_found'
-handler500 = 'views.custom_server_error'
