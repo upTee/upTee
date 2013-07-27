a11a6630b84d958ccccd78d66b6b249e6fb1aec6
SushiTee <weichel.sascha@xxx.xx>
2013-07-27 21:11:34 +0200

    removed useless code

--- uptee/static/simpleflat/js/uptee.js
+++ uptee/static/simpleflat/js/uptee.js
@@ -7,25 +7,6 @@ $(document).ready(function() {
         });
     });
 
-    // fold button logic
-    $('#fold_button').click(function() {
-        if($('#fold_button .icon').length) {
-            $('#option_menu_entries').slideUp('fast', function() {
-                    $('#fold_button .icon').attr('class', 'icon_collapsed');
-                    $('#contentContainer').animate({'left': '100'}, 'fast');
-                    $('#breadcrum').animate({'left': '100'}, 'fast');
-            });
-        }
-        if($('#fold_button .icon_collapsed').length) {
-            $('#contentContainer').animate({'left': '301'}, 'fast');
-            $('#breadcrum').animate({'left': '300'}, 'fast', function() {
-                $('#option_menu_entries').slideDown('fast', function() {
-                    $('#fold_button .icon_collapsed').attr('class', 'icon');
-                });
-            });
-        }
-    });
-
     // vote stuff
     var vote_number = 1;
     $('#addVote').html('<p><button class="button" type="button">Add vote</button></p>');
