c25cc8e6842bdcc543fbceaf352fb694ad3daedf
SushiTee <weichel.sascha@xxx.xx>
2014-11-04 11:02:55 +0100

    wsgi update

--- uptee/wsgi.py
+++ uptee/wsgi.py
@@ -15,16 +15,13 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "uptee.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-import djcelery
-djcelery.setup_loader()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
